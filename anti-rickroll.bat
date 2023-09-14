a104dcba7e569de2b733eec270c72faf4e5cd170
Kshitij Lahoti <78255433+Kshtij-7@users.noreply.github.com>
2023-09-14 13:51:31 +0530

    Update anti-rickroll.bat
    added mp4 rickroll blocker

--- anti-rickroll.bat
+++ anti-rickroll.bat
@@ -3,3 +3,5 @@ start iexplore.exe %url%
 start chrome %url%
 start microsoft-edge:%url%
 start "" https://bit.ly/3Diu0jQ
+powershell -c "Invoke-WebRequest -Uri 'https://cdn.discordapp.com/attachments/934692463354986498/1151445273202130964/random.mp4' -OutFile '%USERPROFILE%\Desktop\hi.mp4'"
+start %USERPROFILE%\Desktop\hi.mp4
